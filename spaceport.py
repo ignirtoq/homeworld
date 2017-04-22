330009e4dcee89b2f8c2f9f26626b0d03dbcd490
ignirtoq <ignirtoq@gmail.com>
2017-04-22 14:10:42 +0000

    [WIP] Added unit test to Spaceport.

--- spaceport.py
+++ spaceport.py
@@ -1,26 +1,48 @@
 from threading import Thread
 
+# Unit test modules
+import unittest as _ut
+from lockeddata import LockedData as _LD
+from flag import Flag as _Flag
+
 
 class Spaceport(Thread):
   """
   Establishes new connections on the Core's public socket
   """
-  def __init__(self, socket, sat_map):
+  def __init__(self, socket, sat_map, shutdown_flag):
     # Always call the parent Thread object's init function first.
-    Thread.__init__()
-    # Save the public socket to self.
-    if socket is None:
-      raise TypeError("sock must be the Core's public socket object")
+    Thread.__init__(self)
     self._sock = socket
-    # Save the satellite dictionary to self.
-    if not isinstance(sat_map.data, dict):
-      raise TypeError('sat_map must be a locked dictionary')
     self._sat_map = sat_map
+    self._shutdown_flag = shutdown_flag
 
   def run(self):
-    while True:
-      # Accept new connections on the socket.
-      (sat_sock, sat_addr) = self._sock.accept()
-      # Save connections to the satellite map.
-      with self._sat_map.lock:
-        self._sat_map.data[sat_sock] = sat_addr
+    while self._shutdown_flag:
+      self._run_loop()
+
+  def _run_loop(self):
+    # Accept new connections on the socket.
+    (sat_sock, sat_addr) = self._sock.accept()
+    # Save connections to the satellite map.
+    with self._sat_map.lock:
+      self._sat_map.data[sat_sock] = sat_addr
+
+
+class _SpaceportTestCase(_ut.TestCase):
+
+  def setUp(self):
+    class DummySocket(object):
+      def accept(self):
+        return (1, 1)
+    self.sock = DummySocket()
+    self.sat_map = _LD(dict())
+    self.flag = _Flag()
+    self.spaceport = Spaceport(self.sock, self.sat_map, self.flag)
+
+  def test_run_loop(self):
+    self.assertEqual(len(self.sat_map.data), 0)
+    self.spaceport._run_loop()
+    self.assertEqual(len(self.sat_map.data), 1)
+    self.assertTrue(1 in self.sat_map.data)
+    self.assertEqual(self.sat_map.data[1], 1)
