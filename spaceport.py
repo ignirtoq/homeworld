07fa24dc101f3aa96674165c218a5bc713e38b45
ignirtoq <ignirtoq@gmail.com>
2017-04-22 15:09:27 +0000

    [WIP] Modified Spaceport to support shutdown flag.
    Now uses select with a timeout (defaults to 0.5 seconds, set in Spaceport
    constructor) to handle Core shutdown flag.

--- spaceport.py
+++ spaceport.py
@@ -1,3 +1,4 @@
+from select import select
 from threading import Thread
 
 # Unit test modules
@@ -10,18 +11,24 @@ class Spaceport(Thread):
   """
   Establishes new connections on the Core's public socket
   """
-  def __init__(self, socket, sat_map, shutdown_flag):
+  def __init__(self, socket, sat_map, shutdown_flag, timeout=0.5):
     # Always call the parent Thread object's init function first.
     Thread.__init__(self)
     self._sock = socket
     self._sat_map = sat_map
     self._shutdown_flag = shutdown_flag
+    self._timeout = timeout
 
   def run(self):
-    while self._shutdown_flag:
+    while not self._shutdown_flag:
       self._run_loop()
 
   def _run_loop(self):
+    rd_list = select([self._sock],[],[], self._timeout)[0]
+    if self._sock in rd_list:
+      self._accept_new_connection()
+
+  def _accept_new_connection(self):
     # Accept new connections on the socket.
     (sat_sock, sat_addr) = self._sock.accept()
     # Save connections to the satellite map.
@@ -32,10 +39,13 @@ class Spaceport(Thread):
 class _SpaceportTestCase(_ut.TestCase):
 
   def setUp(self):
+    global select
     class DummySocket(object):
       def accept(self):
         return (1, 1)
     self.sock = DummySocket()
+    def select(rd_list, wr_list, ex_list, timeout=None):
+      return [self.sock], [], []
     self.sat_map = _LD(dict())
     self.flag = _Flag()
     self.spaceport = Spaceport(self.sock, self.sat_map, self.flag)
